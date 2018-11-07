256a1e66aaa62978b1fbb80dd40d9cb599676ea1
Jacob Bennett <kf7ags@gmail.com>
2018-11-06 18:40:33 -0700

    worked on more base stylings

--- scripts/styling.js
+++ scripts/styling.js
@@ -1,34 +1,34 @@
-(function(){
-    let inputs = document.getElementsByTagName('input');
-    let header = document.getElementsByTagName('h1')[0].clientHeight;
-    // header = header.split('p');
-    // header = Number(header[0]);
-    for (let i = 0; i < inputs.length; i++){
-        inputs[i].style.height = `${header}px`;
-    }
-})();
-(function(){
-    let inputs = document.getElementsByTagName('input');
-    let header = window.getComputedStyle(document.getElementsByTagName('h1')[0]).getPropertyValue('font-size');
-    header = header.split('p');
-    header = Number(header[0]);
-    for (let i = 0; i < inputs.length; i++){
-        inputs[i].style.fontSize = `${header}px`;
-    }
-})();
-(function(){
-    let headers = document.getElementsByTagName('h1');
-    let result = 0;
-    for (let i = 0; i < headers.length; i++){
-        let width = window.getComputedStyle(document.getElementsByTagName('h1')[i]).getPropertyValue('width');
-        width = width.split('p');
-        width = Number(width[0]);
-        if (width > result){
-            result = width;
-        }
-    }
-    let inputs = document.getElementsByTagName('input');
-    for (let x =0; x < inputs.length; x++){
-        inputs[i].style.width = `${result}px`;
-    }
-})();
+// (function(){
+//     let inputs = document.getElementsByTagName('input');
+//     let header = document.getElementsByTagName('h1')[0].clientHeight;
+//     // header = header.split('p');
+//     // header = Number(header[0]);
+//     for (let i = 0; i < inputs.length; i++){
+//         inputs[i].style.height = `${header}px`;
+//     }
+// })();
+// (function(){
+//     let inputs = document.getElementsByTagName('input');
+//     let header = window.getComputedStyle(document.getElementsByTagName('h1')[0]).getPropertyValue('font-size');
+//     header = header.split('p');
+//     header = Number(header[0]);
+//     for (let i = 0; i < inputs.length; i++){
+//         inputs[i].style.fontSize = `${header}px`;
+//     }
+// })();
+// (function(){
+//     let headers = document.getElementsByTagName('h1');
+//     let result = 0;
+//     for (let i = 0; i < headers.length; i++){
+//         let width = window.getComputedStyle(document.getElementsByTagName('h1')[i]).getPropertyValue('width');
+//         width = width.split('p');
+//         width = Number(width[0]);
+//         if (width > result){
+//             result = width;
+//         }
+//     }
+//     let inputs = document.getElementsByTagName('input');
+//     for (let x =0; x < inputs.length; x++){
+//         inputs[i].style.width = `${result}px`;
+//     }
+// })();
