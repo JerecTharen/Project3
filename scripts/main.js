9cac91581559f2e1fdd6e72cea2b670ecea054ed
Jacob Bennett <kf7ags@gmail.com>
2018-11-14 08:19:29 -0700

    modal space set up and can now add players

--- scripts/main.js
+++ scripts/main.js
@@ -119,11 +119,53 @@ class Player{
 }
 console.log('code has loaded');
 
+let players = new AllPlayers([],0);
+
 
 function drawPage(){
-    for (let i = 0; i < 18; i++){
-        document.getElementById(`${i+1}yard`).innerHTML = tee.myYards[i];
-        document.getElementById(`${i+1}handi`).innerHTML = tee.handi[i];
-        document.getElementById(`${i+1}par`).innerHTML = tee.myPar[i];
+    if(tee.id !== null){
+        for (let i = 0; i < 18; i++){
+            document.getElementById(`${i+1}yard`).innerHTML = tee.myYards[i];
+            document.getElementById(`${i+1}handi`).innerHTML = tee.handi[i];
+            document.getElementById(`${i+1}par`).innerHTML = tee.myPar[i];
+        }
+    }
+    document.getElementById('col0').innerHTML = `<p>Hole:</p><p>Yardage:</p><p>Handicap:</p><p>PAR</p>`;
+    document.getElementById('col02').innerHTML = `<p>Hole:</p><p>Yardage:</p><p>Handicap:</p><p>PAR</p>`;
+    for (let x = 0; x < players.players.length;x++){
+        document.getElementById('col0').innerHTML += `<p>${players.players[x].name}</p>`;
+        document.getElementById('col02').innerHTML += `<p>${players.players[x].name}</p>`;
     }
 }
+
+
+function showAddPlayer(){
+    document.getElementById('modalSpace').style.display ='block';
+    document.getElementById('addplayerSpace').style.display = 'block';
+    document.getElementsByClassName('containsAll')[0].blur();
+}
+function addPlayer(){
+    let name = document.getElementById('playerName');
+    if (name.value == 0 || name.value == ''){
+        let messageSpace = document.getElementById('messageSpace');
+        messageSpace.style.display = 'block';
+        messageSpace.innerHTML = '<h3>You did not enter a correct name</h3>';
+
+    }
+    else{
+        players.addPlayer(name.value,playerNum,[],false,0);
+        hideAddPlayer();
+        drawPage();
+        hideMessageSpace();
+        name.value = '';
+    }
+}
+function hideAddPlayer(){
+    document.getElementById('modalSpace').style.display ='none';
+    document.getElementById('addplayerSpace').style.display = 'none';
+    // document.getElementsByClassName('containsAll')[0].blur();
+}
+function hideMessageSpace(){
+    document.getElementById('messageSpace').style.display = 'none';
+}
+
